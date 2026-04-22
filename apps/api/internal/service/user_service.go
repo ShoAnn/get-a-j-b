a6eff23ab489ab466e95b1bd200d3eb2bf45d988
ShoAnn <hikarinoaibou66@gmail.com>
2026-04-22 19:29:08 +0700

    update user_service

--- apps/api/internal/service/user_service.go
+++ apps/api/internal/service/user_service.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/ShoAnn/get-a-j-b/api/internal/domain"
 	"golang.org/x/crypto/bcrypt"
-	"golang.org/x/tools/go/analysis/passes/ifaceassert"
 )
 
 type UserService struct {
@@ -39,6 +38,22 @@ func (s *UserService) Register(ctx context.Context, user *domain.CreateUserReque
 	return newUser, nil
 }
 
+func (s *UserService) GetAllUsers(ctx context.Context) ([]*domain.User, error) {
+	users, err := s.repo.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *UserService) GetUserByID(ctx context.Context, id int) (*domain.User, error) {
+	user, err := s.repo.GetByID(ctx, id)
+	if err != nil {
+		return nil, domain.ErrUserNotFound
+	}
+	return user, nil
+}
+
 func (s *UserService) UpdateUser(ctx context.Context, id int, user *domain.UpdateUserRequest) (*domain.User, error) {
 	existingUser, err := s.repo.GetByID(ctx, id)
 	if err != nil {
@@ -48,8 +63,8 @@ func (s *UserService) UpdateUser(ctx context.Context, id int, user *domain.Updat
 	if user.Username != nil {
 		existingUser.Username = *user.Username
 	}
-	if user.Email != "" && user.Email != existingUser.Email {
-		exist, err := s.repo.ExistByEmail(ctx, user.Email)
+	if user.Email != nil && user.Email != &existingUser.Email {
+		exist, err := s.repo.ExistByEmail(ctx, *user.Email)
 		if err != nil {
 			return nil, err
 		}
@@ -61,12 +76,23 @@ func (s *UserService) UpdateUser(ctx context.Context, id int, user *domain.Updat
 	if err != nil {
 		return nil, err
 	}
+	return updatedUser, nil
+}
 
+func (s *UserService) DeleteUser(ctx context.Context, id int) error {
+	existingUser, err := s.repo.GetByID(ctx, id)
+	if err != nil {
+		return domain.ErrUserNotFound
+	}
+	return s.repo.Delete(ctx, existingUser.ID)
+}
 
+// Helper functions
 func HashPassword(password string) (string, error) {
 	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(hashedBytes), err
 }
+
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
