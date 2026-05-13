93f5e9c8b8f7963c63e7206e7f669e5da89161a3
ShoAnn <hikarinoaibou66@gmail.com>
2026-05-13 07:58:46 +0700

    add resume repository service handler; fix job service

--- apps/api/internal/domain/resume.go
+++ apps/api/internal/domain/resume.go
@@ -21,14 +21,16 @@ type UpdateResumeRequest struct {
 
 type ResumeRepository interface {
 	Create(ctx context.Context, resume *Resume) (*Resume, error)
+	GetAll(ctx context.Context, userID int) ([]*Resume, error)
 	GetByID(ctx context.Context, id int) (*Resume, error)
 	Update(ctx context.Context, resume *Resume) (*Resume, error)
 	Delete(ctx context.Context, id int) error
 }
 
 type ResumeService interface {
-	CreateResume(ctx context.Context, req *CreateResumeRequest) (*Resume, error)
-	GetByID(ctx context.Context, id int) (*Resume, error)
-	UpdateResume(ctx context.Context, id int, req *UpdateResumeRequest) (*Resume, error)
-	DeleteResume(ctx context.Context, id int) error
+	CreateResume(ctx context.Context, userID int, req *CreateResumeRequest) (*Resume, error)
+	GetAllResumes(ctx context.Context, userID int) ([]*Resume, error)
+	GetByID(ctx context.Context, resumeID int, userID int) (*Resume, error)
+	UpdateResume(ctx context.Context, resumeID int, userID int, req *UpdateResumeRequest) (*Resume, error)
+	DeleteResume(ctx context.Context, resumeID int, userID int) error
 }
